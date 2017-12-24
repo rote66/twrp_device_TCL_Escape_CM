053817539415f1ce2f2d6a41e7d482395d7bd303
rote66 <pontifex23872388@gmail.com>
2017-12-24 07:47:55 +0100

    Initial tree

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# twrp_device_TCL_Escape_CM
-For TCL_P620M
+TWRP Device 
+Device:TCL ono/TCL_P620M
+Codename:Escape_CM
+CPU:MT6735
+
+
+##How to compile:
+
+1. Get the latest Lineageos14.1 sources and twrp sources
+2. rm -r system/sepolicy 
+3. git clone http://github.com/rote66/android_system_sepolicy.git -b los-14.1-ono system/sepolicy
+4. ./build/envsetup.sh
+5. lunch lineage_Escape_CM-userdebug
+6. make -jX (where X is the # of your CPU threads +1) recoveryimage
+
+By wyk
---
+++ cm.mk
@@ -0,0 +1,25 @@
+## Specify phone tech before including full_phone
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+include $(call all-subdir-makefiles)
+
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+# Release name
+PRODUCT_RELEASE_NAME := TCL_P620M
+
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Escape_CM
+PRODUCT_NAME := lineage_Escape_CM
+PRODUCT_BRAND := TCL
+PRODUCT_MODEL := TCL_P620M
+PRODUCT_MANUFACTURER := TCL
